5fcc51c1e70e4f7bdcee40ffd25b962a731260bc
Divine Chukwudire <94472610+D1v1n6@users.noreply.github.com>
2025-12-02 22:16:43 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -21,11 +21,12 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PhotoGallery API v1");
+    c.RoutePrefix = string.Empty; // Swagger UI at root
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
